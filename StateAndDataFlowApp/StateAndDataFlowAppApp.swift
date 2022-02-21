a3c9a1cff12bd11faf6ed0cc4e70ce2c39803109
vic8tor <timongrinni@gmail.com>
2022-02-21 20:50:53 +0300

    Initial Commit

---
+++ StateAndDataFlowApp/StateAndDataFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowAppApp.swift
+//  StateAndDataFlowApp
+//
+//  Created by Victor on 21.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
