51aeb5a38c6b8c7c7049f72a1f728b10ae915143
vic8tor <timongrinni@gmail.com>
2022-02-22 18:45:08 +0300

    Create StartView

---
+++ StateAndDataFlowApp/StarterView.swift
@@ -0,0 +1,29 @@
+//
+//  StarterView.swift
+//  StateAndDataFlowApp
+//
+//  Created by Victor on 22.02.2022.
+//
+
+import SwiftUI
+
+struct StarterView: View {
+    @EnvironmentObject var user: UserManager
+    
+    var body: some View {
+        Group {
+            if user.isRegister {
+                ContentView()
+            } else {
+                RegistrerView()
+            }
+        }
+    }
+}
+
+struct StarterView_Previews: PreviewProvider {
+    static var previews: some View {
+        StarterView()
+            .environmentObject(UserManager())
+    }
+}
