69e61c5ecf18526bc40c8b82f20c67ef5affb4ff
vic8tor <timongrinni@gmail.com>
2022-02-23 17:00:19 +0300

    Added UserDefault

---
+++ StateAndDataFlowApp/Managers/UserManager.swift
@@ -0,0 +1,33 @@
+//
+//  UserManager.swift
+//  StateAndDataFlowApp
+//
+//  Created by Victor on 22.02.2022.
+//
+
+import Foundation
+import Combine
+
+
+class UserManager: ObservableObject {
+   
+    @Published var isRegistred: Bool {
+        didSet {
+            UserDefaults.standard.set(isRegistred, forKey: "isRegistred")
+        }
+    }
+    @Published var name: String {
+        didSet {
+            UserDefaults.standard.set(name, forKey: "name")
+        }
+    }
+    
+    init() {
+        self.isRegistred = UserDefaults
+            .standard
+            .object(forKey: "isRegistred") as? Bool ?? false
+        self.name = UserDefaults
+            .standard
+            .object(forKey: "name") as? String ?? ""
+    }
+}
